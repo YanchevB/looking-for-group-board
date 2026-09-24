30cb96ecf8de76e236c3b99faee57d84bb1b4e76
Boyan Yanchev <byanchev6@gmail.com>
2026-09-24 21:52:06 +0300

    feat: create CatalogPage component with styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import Footer from './layout/Footer'
 import Header from './layout/Header'
 import HomePage from './pages/HomePage'
+import CatalogPage from './pages/CatalogPage'
 
 function App() {
 
   return (
     <>
+
     </>
   )
 }
